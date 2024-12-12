bdc52c7e105188cb0d2b89680189379f33303f05
Iuri Cernov <iuri.cernov@gmail.com>
2024-12-12 01:08:10 -0400

    feat: downloading zip file

--- ios/Bundlepush.mm
+++ ios/Bundlepush.mm
@@ -1,13 +1,37 @@
 #import "Bundlepush.h"
 #import <Foundation/Foundation.h>
+#import <CommonCrypto/CommonDigest.h>
 
 
 @implementation Bundlepush
 RCT_EXPORT_MODULE()
 
+NSString *MD5HashOfFile(NSString *filePath) {
+    // Load the file data into memory
+  NSError *error;
+    NSData *fileData = [NSData dataWithContentsOfFile:filePath options:0 error:&error];
+    if (!fileData) {
+        NSLog(@"Failed to read file at path: %@", filePath);
+      NSLog(@"%@", error);
+        return nil;
+    }
+    
+    // Create the MD5 hash
+    unsigned char md5Buffer[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(fileData.bytes, (CC_LONG)fileData.length, md5Buffer);
+    
+    // Convert to a hexadecimal string
+    NSMutableString *md5String = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for (int i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [md5String appendFormat:@"%02x", md5Buffer[i]];
+    }
+    
+    return [md5String copy];
+}
 
 - (void)downloadFileFromURL:(NSURL *)url
               toDestination:(NSURL *)destinationURL
+          completionHandler:(void (^)())completionHandler
 {
     NSURLSession *session = [NSURLSession sharedSession];
     NSLog(@"Starting download from: %@", [url absoluteString]);
@@ -22,9 +46,10 @@ RCT_EXPORT_MODULE()
                                                 toURL:destinationURL
                                                 error:&fileError];
         if (fileError) {
-            NSLog(@"File move error: %@", fileError.localizedDescription);
+          NSLog(@"File move error: %@", fileError.localizedDescription);
         } else {
-            NSLog(@"File downloaded successfully to %@", location);
+          NSLog(@"File downloaded successfully to %@", destinationURL);
+          completionHandler();
         }
     }];
 
@@ -32,25 +57,59 @@ RCT_EXPORT_MODULE()
     [downloadTask resume];
 }
 
-- (NSURL *)fileURLInDocumentsDirectoryWithFileName:(NSString *)filename {
-    // Get the Documents directory path
-    NSArray<NSURL *> *documentDirectories = [[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory
-                                                                                   inDomains:NSUserDomainMask];
+- (BOOL)checkBundleFolderAvailable
+{
+  NSURL *workdir = [[[NSBundle mainBundle] bundleURL] URLByAppendingPathComponent:@"bp_workdir" isDirectory:YES];
 
-    // The Documents directory for the app
-    NSURL *documentsDirectory = [documentDirectories firstObject];
-
-    // Append the file name to create the full file URL
-    NSURL *fileURL = [documentsDirectory URLByAppendingPathComponent:filename];
-    return fileURL;
+  BOOL isDirectory;
+  BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:[workdir absoluteString]
+                                                     isDirectory:&isDirectory];
+  if (exists) {
+    return isDirectory;
+  }
+  NSError *error = nil;
+  BOOL result = [[NSFileManager defaultManager] createDirectoryAtURL:workdir
+                                         withIntermediateDirectories:YES
+                                                          attributes:nil
+                                                               error:&error];
+  if (!result) {
+    NSLog(@"Error creating folder - %@", error);
+  }
+  return result;
 }
 
 - (void)performOTACheck
 {
-  NSURL *downloadURL = [NSURL URLWithString:@"https://iuri.s3.us-east-1.amazonaws.com/main.jsbundle"];
-  NSURL *destination = [self fileURLInDocumentsDirectoryWithFileName:@"bundle-result.jsbundle"];
-  NSLog(@"File downloaded to %@", destination);
-  [self downloadFileFromURL:downloadURL toDestination:destination];
+  // Check if there's a bundle not extracted (bp_workdir/latest.zip)
+  //   Extract it immediately and apply
+  //   Rename (bp_workdir/applied.zip)
+  // Async:
+  //   Perform API call to check for the latest bundle
+  //   If available:
+  //     Check if md5 matches bp_workdir/latest.zip or bp_workdir/applied.zip
+  //     If matches, stop flow
+  //     Download the new bundle to bp_workdir/downloaded.zip *
+  //     Check if md5 matches - if so, rename to bp_workdir/latest.zip
+
+  BOOL available = [self checkBundleFolderAvailable];
+  if (!available) {
+    return;
+  }
+  
+  NSURL *downloadDestination = [[[NSBundle mainBundle] bundleURL] URLByAppendingPathComponent:@"bp_workdir/downloaded.zip"];
+  NSURL *downloadURL = [NSURL URLWithString:@"https://iuri.s3.us-east-1.amazonaws.com/bundle.zip"];
+  NSLog(@"File will be downloaded to %@", downloadDestination);
+  [self downloadFileFromURL:downloadURL
+              toDestination:downloadDestination
+          completionHandler:^() {
+    NSString *md5 = MD5HashOfFile([downloadDestination path]);
+    NSLog(@"Downloaded file md5 = %@", md5);
+    // Add if to check if hashes match
+    NSURL *latest = [[[NSBundle mainBundle] bundleURL] URLByAppendingPathComponent:@"bp_workdir/latest.zip"];
+    [[NSFileManager defaultManager] moveItemAtURL:downloadDestination
+                                            toURL:latest
+                                            error:nil];
+  }];
 }
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
