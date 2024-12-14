d1934c991ca50b7141852a0e62774a1c7c22bdbb
Iuri Cernov <iuri.cernov@gmail.com>
2024-12-14 17:54:43 -0400

    feat: verify md5 for safety

--- ios/BundlepushNative.mm
+++ ios/BundlepushNative.mm
@@ -10,8 +10,8 @@ NSString *MD5HashOfFile(NSString *filePath) {
   NSError *error;
   NSData *fileData = [NSData dataWithContentsOfFile:filePath options:0 error:&error];
   if (!fileData) {
-    NSLog(@"Failed to read file at path: %@", filePath);
-    NSLog(@"%@", error);
+    BPLog(@"Failed to read file at path: %@", filePath);
+    BPLog(@"%@", error);
     return nil;
   }
   
@@ -24,6 +24,13 @@ NSString *MD5HashOfFile(NSString *filePath) {
   }
   
   return [md5String copy];
+}
+
+void BPLog(NSString *format, ...) {
+  va_list args;
+  va_start(args, format);
+  NSLog(@"[BundlePush] %@", [[NSString alloc] initWithFormat:format arguments:args]);
+  va_end(args);
 }
 
 + (NSString *)getBuildNumber
@@ -51,7 +58,7 @@ NSString *MD5HashOfFile(NSString *filePath) {
   NSURLSessionDataTask *dataTask = [session dataTaskWithURL:url
                                           completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
     if (error) {
-      NSLog(@"Error: %@", error.localizedDescription);
+      BPLog(@"Error: %@", error.localizedDescription);
       return;
     }
 
@@ -59,14 +66,14 @@ NSString *MD5HashOfFile(NSString *filePath) {
       NSError *jsonError;
       NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
       if (jsonError) {
-        NSLog(@"JSON Parsing Error: %@", jsonError.localizedDescription);
+        BPLog(@"JSON Parsing Error: %@", jsonError.localizedDescription);
       } else {
         NSString *md5 = [json objectForKey:@"md5"];
         NSString *bundle = [json objectForKey:@"url"];
         if (md5 && bundle) {
           completionHandler(md5, [NSURL URLWithString:bundle]);
         } else {
-          NSLog(@"No bundle updates available");
+          BPLog(@"No bundle updates available");
         }
       }
     }
@@ -80,11 +87,11 @@ NSString *MD5HashOfFile(NSString *filePath) {
           completionHandler:(void (^)())completionHandler
 {
   NSURLSession *session = [NSURLSession sharedSession];
-  NSLog(@"Starting download from: %@", [url absoluteString]);
+  BPLog(@"Starting download from: %@", [url absoluteString]);
   NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithURL:url
                                                       completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
     if (error) {
-      NSLog(@"Download error: %@", error.localizedDescription);
+      BPLog(@"Download error: %@", error.localizedDescription);
       return;
     }
     [[NSFileManager defaultManager] removeItemAtURL:destinationURL
@@ -95,9 +102,9 @@ NSString *MD5HashOfFile(NSString *filePath) {
                                             toURL:destinationURL
                                             error:&fileError];
     if (fileError) {
-      NSLog(@"File move error: %@", fileError.localizedDescription);
+      BPLog(@"File move error: %@", fileError.localizedDescription);
     } else {
-      NSLog(@"File downloaded successfully to %@", destinationURL);
+      BPLog(@"File downloaded successfully to %@", destinationURL);
       completionHandler();
     }
   }];
@@ -118,7 +125,7 @@ NSString *MD5HashOfFile(NSString *filePath) {
                                                           attributes:nil
                                                                error:&error];
   if (!result) {
-    NSLog(@"Error creating folder - %@", error);
+    BPLog(@"Error creating folder - %@", error);
   }
   return result;
 }
@@ -131,15 +138,13 @@ NSString *MD5HashOfFile(NSString *filePath) {
   // Async:
   //   Perform API call to check for the latest bundle *
   //   If available: *
-  //     Check if md5 matches bp_workdir/<version_code>/current.zip (PENDING)
-  //     If doesn't match, stop flow. Otherwise continue (PENDING)
+  //     Check if md5 matches bp_workdir/<version_code>/current.zip *
+  //     If matches, stop flow, it's not necessary to download again. Otherwise continue *
   //     Download the new bundle to bp_workdir/<version_code>/downloaded.zip *
-  //     Check if md5 matches - if so (PENDING)
+  //     Check if md5 matches - if so *
   //       Extract it to bp_workdir/<version_code>/current_bundle *
   //       Rename it to bp_workdir/<version_code>/current.zip *
   
-  // TODO standardize logs
-
   BOOL available = [self checkBundleFolderAvailable];
   if (!available) {
     return;
@@ -148,22 +153,24 @@ NSString *MD5HashOfFile(NSString *filePath) {
   [self requestBundleForAppId:@"app-id"
         withCompletionHandler:^(NSString *md5, NSURL *bundleURL) {
     NSURL *currentZip = [[self workdir] URLByAppendingPathComponent:@"current.zip"];
-    NSString *currentZipMD5 = MD5HashOfFile([currentZip path]);
+    NSString *currentZipMd5 = MD5HashOfFile([currentZip path]);
     
-    if ([currentZipMD5 isEqualToString:md5]) {
-      NSLog(@"Current bundle is up to date with the server (md5 = %@)", md5);
+    if ([currentZipMd5 isEqualToString:md5]) {
+      BPLog(@"Current bundle is up to date with the server (md5 = %@)", md5);
       return;
     }
     
     // Download the new bundle to bp_workdir/<version_code>/downloaded.zip
     NSURL *downloadedZip = [[self workdir] URLByAppendingPathComponent:@"downloaded.zip"];
-    NSLog(@"File will be downloaded to %@", downloadedZip);
+    BPLog(@"File will be downloaded to %@", downloadedZip);
     [self downloadFileFromURL:bundleURL
                 toDestination:downloadedZip
             completionHandler:^() {
       NSString *downloadMd5 = MD5HashOfFile([downloadedZip path]);
-      NSLog(@"Downloaded file md5 = %@ - expected is: %@", downloadMd5, md5);
-      // TODO Add if to check if hashes match
+      if (![downloadMd5 isEqualToString:md5]) {
+        BPLog(@"Aborting bundle install - downloadeded file md5 = %@ - expected is: %@", downloadMd5, md5);
+        return;
+      }
       NSURL *extractPath = [[self workdir] URLByAppendingPathComponent:@"current_bundle" isDirectory:YES];
       [[NSFileManager defaultManager] removeItemAtURL:extractPath error:nil];
       NSError *zipError = nil;
@@ -173,7 +180,7 @@ NSString *MD5HashOfFile(NSString *filePath) {
                                                password:nil
                                                   error:&zipError];
       if (!successUnzip) {
-        NSLog(@"Error unzipping - %@", zipError);
+        BPLog(@"Error unzipping - %@", zipError);
         return;
       }
       
@@ -191,7 +198,7 @@ NSString *MD5HashOfFile(NSString *filePath) {
   BOOL isDirectory;
   BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:[bundleUrl path]
                                                      isDirectory:&isDirectory];
-  NSLog(@"Latest bundle: %@", bundleUrl);
+  BPLog(@"Latest bundle: %@", bundleUrl);
   if (exists && !isDirectory) {
     return bundleUrl;
   } else {
