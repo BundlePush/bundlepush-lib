0d697e3d3520771f2515b43d958f90b174f7d5d2
Iuri Cernov <iuri.cernov@gmail.com>
2025-02-08 00:44:23 -0400

    feat: generating bundle

--- src/bin/bundlepush/release/index.js
+++ src/bin/bundlepush/release/index.js
@@ -2,6 +2,11 @@ import { uploadFile } from '../service/aws.js';
 import { createBundle, requestUploadUrl } from '../service/bundle.js';
 import { getCurrentAuthState } from '../utils/getAuthState.js';
 import md5File from 'md5-file';
+import os from 'os';
+import fs from 'fs';
+import path from 'path';
+import { execa } from 'execa';
+import zipper from 'zip-local';
 
 export async function handleRelease(args) {
   const { app } = args;
@@ -17,8 +22,36 @@ export async function handleRelease(args) {
   }
 
   // TODO check if app is from organization
+  // TODO retrieve app's platform
 
-  // TODO Step 1: build the app
+  // Step 1.1: get a temporary directory
+  const bundleDirectory = fs.mkdtempSync(path.join(os.tmpdir(), 'bp-'));
+
+  // Step 1.2: generate the bundle
+  const execResult = await execa('npx', [
+    'react-native',
+    'bundle',
+    '--platform',
+    'ios',
+    '--dev',
+    'false',
+    '--entry-file',
+    'index.js',
+    '--bundle-output',
+    `${bundleDirectory}/main.jsbundle`,
+    '--assets-dest',
+    bundleDirectory,
+  ]);
+  if (execResult.exitCode !== 0) {
+    console.error('Failed to generate the bundle');
+    console.error(execResult.stderr);
+    process.exit(1);
+  }
+
+  const bundleFile = `${bundleDirectory}/bundle.zip`;
+
+  // Step 1.3: zip the bundle
+  zipper.sync.zip(bundleDirectory).compress().save(bundleFile);
 
   // Step 2: request upload key
   const uploadUrlData = await requestUploadUrl({
@@ -29,10 +62,10 @@ export async function handleRelease(args) {
   // Step 3: upload the bundle
   await uploadFile({
     presignedUrl: uploadUrlData.url,
-    localFile: 'package-lock.json', // TODO use correct file
+    localFile: bundleFile,
   });
 
-  const md5 = await md5File('package-lock.json');
+  const md5 = await md5File(bundleFile);
 
   // Step 4: create the bundle
   await createBundle({
