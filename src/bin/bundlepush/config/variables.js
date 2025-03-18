290f07ca87efd3964d4ac350acc91efc7c0105c5
Iuri <iuri@cernovapps.com>
2025-03-18 18:43:56 -0300

    feat: open correct url address

--- src/bin/bundlepush/config/variables.js
+++ src/bin/bundlepush/config/variables.js
@@ -1,4 +1,5 @@
-export const API_KEYS_URL = 'https://bundlepu.sh';
+export const API_KEYS_URL =
+  'https://dash.bundlepu.sh/dashboard/organization#api-keys';
 export const BUNDLEPUSH_API_KEY = process.env.BUNDLEPUSH_API_KEY;
 export const CONFIG_DIRECTORY = '.bundlepush';
 export const BASE_URL = 'https://api.bundlepu.sh';
